5958da399c3213591389053cda568a3d365896a0
Kevin Skayro <kev1n-1997@outlook.com>
2020-11-04 10:24:11 -0500

    added functionality to bottom explore btn

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 //Selectors
+//Burger menu selectors
 const burgerBtn = document.querySelector(".burger-menu");
 const BurgerMenu = document.querySelector(".top-settings-menu");
+const BurgerMenuWrap = document.querySelector(".top-menu-wrap");
+//bottom menu selectors
+const homeBtn = document.querySelector(".fa-home");
+const searchBtn = document.querySelector(".fa-search");
+const exploreBtn = document.querySelector(".fa-ellipsis-h");
+const exploreWindow = document.querySelector(
+  ".bottom-main-menu-container-wrap"
+);
 //Event listeners
 burgerBtn.addEventListener("click", () => {
   BurgerMenu.classList.toggle("active");
   burgerBtn.classList.toggle("active");
+  BurgerMenuWrap.classList.toggle("active");
+});
+exploreBtn.addEventListener("click", () => {
+  exploreWindow.classList.toggle("active");
 });
 //Functions
