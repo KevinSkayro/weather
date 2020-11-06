d2450c5a5649f6efcee1ce8046a9d58d24b9a2a4
Kevin Skayro <kev1n-1997@outlook.com>
2020-11-06 10:53:42 -0500

    added functionality to search button and search input

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+const api = {
+  key: "249e0914f0284c25bda121440200410",
+  base: "http://api.weatherapi.com/v1",
+};
 //Selectors
 //Burger menu selectors
 const burgerBtn = document.querySelector(".burger-menu");
 const BurgerMenu = document.querySelector(".top-settings-menu");
 const BurgerMenuWrap = document.querySelector(".top-menu-wrap");
 //bottom menu selectors
+const bottomMainContainer = document.querySelector(
+  ".bottom-main-menu-container"
+);
 const homeBtn = document.querySelector(".fa-home");
-const searchBtn = document.querySelector(".fa-search");
+const returnBtn = document.querySelector(".fa-chevron-left");
+const searchBtn = document.querySelector(".searchActivatorBtn");
+const activeSearchBtn = document.querySelector(".activeSearchBtn");
 const exploreBtn = document.querySelector(".fa-ellipsis-h");
 const exploreWindow = document.querySelector(
   ".bottom-main-menu-container-wrap"
 );
+const userInput = document.querySelector(".user-input");
 //Event listeners
 burgerBtn.addEventListener("click", () => {
   BurgerMenu.classList.toggle("active");
@@ -19,4 +29,27 @@ burgerBtn.addEventListener("click", () => {
 exploreBtn.addEventListener("click", () => {
   exploreWindow.classList.toggle("active");
 });
+searchBtn.addEventListener("click", () => {
+  bottomMainContainer.classList.toggle("activeSearch");
+});
+returnBtn.addEventListener("click", () => {
+  bottomMainContainer.classList.toggle("activeSearch");
+});
+activeSearchBtn.addEventListener("click", getQuery);
+
 //Functions
+
+function getQuery(e) {
+  e.preventDefault();
+  updateSearch(userInput.value);
+}
+function updateSearch(query) {
+  fetch(`${api.base}/forecast.json?key=${api.key}&q=${query}&days=3`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayForecast);
+}
+function displayForecast(weather) {
+  console.log(weather.forecast);
+}
