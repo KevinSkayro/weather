430f0fbffa8953b99ef988e25b4cb773eb613b55
Kevin Skayro <kev1n-1997@outlook.com>
2020-11-10 10:27:22 -0500

    added fist version of celsius to fahrenheit switch

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const api = {
 //Selectors
 //Burger menu selectors
 const burgerBtn = document.querySelector(".burger-menu");
-const BurgerMenu = document.querySelector(".top-settings-menu");
+const burgerMenu = document.querySelector(".top-settings-menu");
+const celsiusBtn = document.querySelector(".celsius");
+const fahrenheitBtn = document.querySelector(".fahrenheit");
 //Bottom menu selectors
 const bottomMainContainer = document.querySelector(
   ".bottom-main-menu-container"
@@ -20,6 +22,7 @@ const exploreWindow = document.querySelector(
 );
 const userInput = document.querySelector(".user-input");
 //Main Body selectors
+const maincontainer = document.querySelector(".mid-section");
 const city = document.querySelector(".city");
 const state = document.querySelector(".state");
 const date = document.querySelector(".date");
@@ -29,7 +32,7 @@ const weatherCondition = document.querySelector(".weather-state-container");
 
 //Event listeners
 burgerBtn.addEventListener("click", () => {
-  BurgerMenu.classList.toggle("active");
+  burgerMenu.classList.toggle("active");
   burgerBtn.classList.toggle("active");
 });
 exploreBtn.addEventListener("click", () => {
@@ -43,11 +46,34 @@ returnBtn.addEventListener("click", () => {
 });
 activeSearchBtn.addEventListener("click", getQuery);
 
+celsiusBtn.addEventListener("click", () => {
+  maincontainer.classList.add("celsius");
+  celsiusBtn.style.background = "#000";
+  celsiusBtn.style.color = "#fff";
+  celsiusBtn.style.border = "none";
+  fahrenheitBtn.style.background = "#fff";
+  fahrenheitBtn.style.color = "#000";
+  fahrenheitBtn.style.border = "1px solid #000";
+});
+fahrenheitBtn.addEventListener("click", () => {
+  maincontainer.classList.add("celsius");
+  fahrenheitBtn.style.background = "#000";
+  fahrenheitBtn.style.color = "#fff";
+  fahrenheitBtn.style.border = "none";
+  celsiusBtn.style.background = "#fff";
+  celsiusBtn.style.color = "#000";
+  celsiusBtn.style.border = "1px solid #000";
+  maincontainer.classList.remove("celsius");
+});
 //Functions
 
 function getQuery(e) {
   e.preventDefault();
-  updateSearch(userInput.value);
+  if (maincontainer.classList.contains("celsius")) {
+    updateSearchcel(userInput.value);
+  } else {
+    updateSearch(userInput.value);
+  }
 }
 function updateSearch(query) {
   fetch(`${api.base}/forecast.json?key=${api.key}&q=${query}&days=3`)
@@ -65,6 +91,27 @@ function displayForecast(weather) {
   feelsLikeTemp.innerHTML = `Feels like ${Math.round(
     weather.current.feelslike_f
   )}&deg;F`;
+  weatherCondition.innerHTML = `<img src="${weather.current.condition.icon}" alt="weather state icon"/> ${weather.current.condition.text}`;
+  bottomMainContainer.classList.toggle("activeSearch");
+  userInput.value = "";
+}
+
+function updateSearchcel(query) {
+  fetch(`${api.base}/forecast.json?key=${api.key}&q=${query}&days=3`)
+    .then((weatherc) => {
+      return weatherc.json();
+    })
+    .then(displayForecastc);
+}
+function displayForecastc(weather) {
+  console.log(weather);
+  city.innerText = `${weather.location.name}`;
+  state.innerText = `${weather.location.region}`;
+  date.innerText = `${weather.forecast.forecastday[0].date}`;
+  tempeture.innerHTML = `${Math.round(weather.current.temp_c)}&deg;C`;
+  feelsLikeTemp.innerHTML = `Feels like ${Math.round(
+    weather.current.feelslike_c
+  )}&deg;C`;
   weatherCondition.innerHTML = `<img src="${weather.current.condition.icon}" alt="weather state icon"/> ${weather.current.condition.text}`;
   bottomMainContainer.classList.toggle("activeSearch");
   userInput.value = "";
